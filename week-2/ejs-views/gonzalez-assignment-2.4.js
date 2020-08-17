9cb4a1faeefabcd4cfd4c7f9555bde961e3d5ee9
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-08-16 19:53:21 -0700

    c

---
+++ week-2/ejs-views/gonzalez-assignment-2.4.js
@@ -0,0 +1,25 @@
+var http = require("http");
+
+var express = require("express");
+
+var path = require("path");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine" , "ejs");
+
+app.get("/", function(request, response){
+    response.render("index", {
+        firstName: "Juvenal",
+        lastName: "Gonzalez",
+        address: "123 Elm St Stockton, CA, 95210"
+    });
+
+});
+
+http.createServer(app).listen(8080, function(){
+    console.log("EJS-Views app started on port 8080.");
+});
+
