c819694fe4486c004dcc49ebba59e2ff000440f2
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-08-24 15:00:58 -0700

    c

---
+++ week-3/morgan-loggin/gonzalez-exercise-3.2.js
@@ -0,0 +1,22 @@
+var express = requie('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+//tell js where to find views directory
+app.set('views', path.resolve(_dirname, 'views'));
+//set ejs view engine
+app.set('viw engine', 'ejs');
+//logger set up
+app.use(logger('dev'));
+//get request that listens to request and renders message as output
+app.get('/', function(req,res){
+    res.render('index', {
+        message: "Morgan Logger Example message here!"
+    });
+});
+//intializes server that is listening on port 3000
+http.createServer(app).listen(3000, function() {
+    console.log('Application started and listening on port %s', 3000)
+});
