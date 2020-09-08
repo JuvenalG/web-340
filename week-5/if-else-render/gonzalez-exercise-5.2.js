d0ec6972314bcd31c58fef34b5dc2fd77a11d282
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2020-09-08 13:05:02 -0700

    c

---
+++ week-5/if-else-render/gonzalez-exercise-5.2.js
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title:  Assignment 5.2
+; Author: Richard Krasso
+; Date:   September 8 2020
+; Modified By: Juvenal Gonzalez
+; Description: Create a web server that passes an array and
+; is outputted an using EJS for loop 
+;===========================================
+*/
+
+const header = require('/users/mrjuv/bu-webdev/web-340/gonzalez-header');
+console.log(header.display("Juvenal", "Gonzalez", "Assignment 3.4"));
+
+//require statements
+var express = require("express");
+var http = require('http');
+var path = require("path");
+//app methods and set path to views folder
+var app = express();
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+//composer object
+var composers = [
+    "Bach",
+    "Mozart",
+    "Beethoven",
+    "Verdi",
+]
+
+
+//routes
+app.get("/", function (req, res){
+    res.render("index", {
+        names: composers
+    });
+});
+
+http.createServer(app).listen(3000, function () {
+    console.log("Application started and listening on port 3000")
+});
